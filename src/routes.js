dec652b47c23bc11adb9def126dd6dca6209e9b7
hmoon630 <hmoon630@github.com>
2019-12-20 09:50:36 +0900

    [UPDATE] Route Token Refreshing Module

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import { Register, Login, ConfirmEmail } from './controllers/auth';
+import { RefreshToken } from './controllers/user';
 
 const auth = new Router;
 
@@ -7,9 +8,15 @@ auth.post('/register/:authority', Register);
 auth.post('/login/:authority', Login);
 auth.get('/verification', ConfirmEmail);
 
+const user = new Router;
+
+user.get('/refresh', RefreshToken)
+
 
 const routes = new Router;
 
 routes.use('/auth', auth.routes());
+routes.use('/user', user.routes());
+
 
 export default routes;
