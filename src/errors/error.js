c0dcf751fc12b4dee7714f9cf41ed93308df2123
hmoon630 <hmoon630@github.com>
2019-12-22 16:56:02 +0900

    [ADD] Add Laptop Errors

--- src/errors/error.js
+++ src/errors/error.js
@@ -31,5 +31,41 @@ module.exports.UNVERIFIED_ACCOUNT = {
 module.exports.INVALID_REQUEST_DATA = {
     statusCode: 422,
     code: 'INVALID_REQUEST_DATA',
-    message: 'The requested data has invalid data'
+    message: 'The requested data has invalid data.'
+}
+
+module.exports.RESERVED_USER = {
+    statusCode: 422,
+    code: "RESERVED_USER",
+    message: 'The user already reserved seat today.'
+}
+
+module.exports.RESERVED_SEAT = {
+    statusCode: 422,
+    code: 'RESERVED_SEAT',
+    message: 'The requested seat is already reserved.'
+}
+
+module.exports.INVALID_SEAT = {
+    statusCode: 422,
+    code: 'INVALID_SEAT',
+    message: 'The requested seat cannot be reseved.'
+}
+
+module.exports.INVALID_APPLY_TIME = {
+    statusCode: 422,
+    code: 'INVALID_APPLY_TIME',
+    message: 'It\'s not available to reserve now.'
+}
+
+module.exports.BORROW_BLOCKED = {
+    statusCode: 422,
+    code: 'BORROW_BLOCKED',
+    message: 'The user is forbidden to borrow laptop now.'
+}
+
+module.exports.NOT_BROUGHT = {
+    statusCode: 422,
+    code: 'NOT_BROUGHT',
+    message: 'The user didn\'t borrow laptop today.'
 }
