07d0cbc6431c194a9c97d4e628fda38390a3804c
hmoon630 <s19017@gsm.hs.kr>
2019-12-26 14:10:36 +0900

    [ADD] Route Laptop Block Module

--- src/router/managerRouter.js
+++ src/router/managerRouter.js
@@ -1,9 +1,12 @@
 import Router from 'koa-router';
 import { RoomDetail } from 'controllers/laptop';
+import { BlockLaptop } from 'controllers/laptopBlock';
 
 const laptop = new Router;
 
 laptop.get('/rooms', RoomDetail);
+
+laptop.post('/block', BlockLaptop);
 
 
 const managerRouter = new Router;
